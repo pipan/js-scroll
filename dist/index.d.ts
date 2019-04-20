fa68a9e43695d30aed77beda6c494fdc25035d63
Peter Gasparik <gasparik.pe@gmail.com>
2019-04-20 23:42:21 +0200

    production build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,4 @@ export { ScrollBox } from './ScrollBox';
 export { ScrollModule } from './ScrollModule';
 export { ScrollMarkBuilder } from './ScrollMarkBuilder';
 export { ScrollBarBuilder } from './ScrollBarBuilder';
+export { Builder } from './Builder';
